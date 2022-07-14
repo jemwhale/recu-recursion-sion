20cf1e093b9ff1183762d73345bf92dd50872ebf
nimonian <nimon@hotmail.co.uk>
2022-07-14 16:23:21 +0100

    adds a stupid example

---
+++ examples/00-storyTime.js
@@ -0,0 +1,16 @@
+/*
+Just a stupid example.
+*/
+
+function storyTime (n) {
+  if (n == 0) {
+    return '"Beans!"\n\n'
+  }
+  return `"There were three men sat around a fire, and one man said, "Tell us a story, Jack!" So Jack said:\n${storyTime(n-1)}"`
+}
+
+console.log(storyTime(1))
+console.log('')
+console.log(storyTime(2))
+console.log('')
+console.log(storyTime(3))
